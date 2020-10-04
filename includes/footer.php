68e7143e957c5c021f9e9a69e33282fbfc51c0e6
Caroline Hagan <caroline@blueocto.co.uk>
2020-10-04 15:04:55 +0100

    Moved Footer section to a part/include

---
+++ includes/footer.php
@@ -0,0 +1,55 @@
+<!-- footer -->
+<div id="footer" class="clearfix">
+	<div id="footer-inner">
+		<div id="footer-left" class="clearfix">
+			<div id="ctl00_g_210cefdb_c7e3_4268_a5d1_d8594a316a9f" __MarkupType="vsattributemarkup" __WebPartId="{210cefdb-c7e3-4268-a5d1-d8594a316a9f}" WebPart="true">
+				<div class="links-box">
+					<ul>
+						<li class="heading">Information</li>
+						<li><a href="/Information-and-advice/Pages/Local-Resilience-Forum.aspx" target="_blank">Preparing for Emergencies</a></li>
+						<li><a href="/About-Us/Transparency-and-Integrity-Programme/Pages/default.aspx" target="_blank">Transparency &amp; Integrity Programme</a></li>
+					</ul>
+				</div>
+				<div class="links-box">
+					<ul>
+						<li class="heading">Legal</li>
+						<li><a href="/pages/privacy-policy.aspx" target="_blank">Cookies</a></li>
+						<li><a href="/About-Us/Freedom-of-information/General/Pages/Privacy-Notice.aspx" target="_blank">Privacy Notice</a></li>
+						<li><a href="/Pages/Terms-and-Conditions-of-Use.aspx" target="_blank">Terms &amp; Conditions of Use</a></li>
+						<li><a href="/Pages/E-Mail%20and%20SMS%20Text%20Disclaimer.aspx" target="_blank">Email &amp; SMS Text Disclaimer</a></li>
+					</ul>
+				</div>
+				<p>​ </p>
+			</div>
+		</div>
+		
+		<!--
+		<div id="footer-right" class="clearfix">
+			<div class="footer-social-l">Find us on</div>
+			<div class="footer-social-r clearfix">
+				<div class="footer-social-icon">
+					<a href="http://www.flickr.com/photos/durhamconstabularyinpix" target="_blank" title="Durham Constabulary on Flickr">
+						<img src="/Style%20Library/img/footer-icn-flickr.jpg" alt="Flickr" />
+					</a>
+				</div>
+				<div class="footer-social-icon">
+					<a href="http://www.youtube.com/durhampolice999" target="_blank" title="Durham Constabulary on YouTube">
+						<img src="/Style%20Library/img/footer-icn-youtube.jpg" alt="YouTube" />
+					</a>
+				</div>
+				<div class="footer-social-icon">
+					<a href="http://twitter.com/DurhamPolice" target="_blank" title="Durham Constabulary on Twitter">
+						<img src="/Style%20Library/img/footer-icn-twitter.jpg" alt="Twitter" />
+					</a>
+				</div>
+				<div class="footer-social-icon">
+					<a href="http://www.facebook.com/pages/Durham-Constabulary/224330430978915" target="_blank" title="Durham Constabulary on Facebook">
+						<img src="/Style%20Library/img/footer-icn-facebook.jpg" alt="Facebook" />
+					</a>
+				</div>
+			</div>
+		</div>
+		-->
+
+	</div>
+</div>
